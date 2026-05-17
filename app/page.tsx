b154b6683587af135f397c8821f8f1f6af9da072
Tunji <macchupiccchu2003@gmail.com>
2026-05-18 00:10:44 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -91,7 +91,7 @@ export default function Home() {
     async function run(){
 
       await typeText(
-        "Loading...",
+        "Loading",
         setLoadingText
       );
 
@@ -117,7 +117,7 @@ export default function Home() {
       );
 
       await backspace(
-        "Loading...",
+        "Loading",
         setLoadingText
       );
 
@@ -351,7 +351,7 @@ transition-all
 
 >
 
-See my portfolio ↓
+See my portfolio
 
 </button>
 
