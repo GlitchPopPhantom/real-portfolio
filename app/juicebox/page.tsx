be65c235eb61f6b9af9d2532829ceb13dab16ee7
Tunji <macchupiccchu2003@gmail.com>
2026-05-20 20:28:11 +0100

    Update page.tsx

--- app/juicebox/page.tsx
+++ app/juicebox/page.tsx
@@ -1,17 +1,78 @@
 "use client";
-import InstrumentForge from '@/components/InstrumentForge';
+
+import InstrumentForge from "@/components/InstrumentForge";
+import Link from "next/link";
 
 export default function JuiceboxHome() {
+
   return (
-    <main className="min-h-screen bg-black flex flex-col items-center justify-center p-4">
-      {/* This is the heart of the site */}
-      <div className="w-full max-w-4xl">
-        <InstrumentForge />
+
+    <main className="
+      min-h-screen
+      bg-black
+      p-4
+      text-white
+      relative
+    ">
+
+      {/* Back button */}
+
+      <Link
+        href="/"
+        className="
+        absolute
+        top-6
+        left-6
+        border
+        border-[#00FF41]/30
+        px-4
+        py-2
+        text-[#00FF41]
+        hover:bg-[#00FF41]/10
+        transition-all
+        font-mono
+        "
+      >
+        ← Back
+      </Link>
+
+
+      <div className="
+        flex
+        justify-center
+        items-center
+        min-h-screen
+      ">
+
+        <div className="
+          w-full
+          max-w-6xl
+        ">
+
+          <InstrumentForge/>
+
+        </div>
+
       </div>
-      
-      <footer className="mt-8 text-[10px] font-mono text-[#00FF41] opacity-40">
+
+
+      <footer
+        className="
+        text-center
+        text-[10px]
+        text-[#00FF41]
+        opacity-40
+        font-mono
+        pb-6
+        "
+      >
+
         JUICEBOX ENGINE // CORE_V1.0
+
       </footer>
+
     </main>
+
   );
+
 }
