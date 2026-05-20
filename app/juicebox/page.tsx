283182534aa493a7c5b747c8b7774ea9b8aafc13
Tunji <macchupiccchu2003@gmail.com>
2026-05-20 21:34:15 +0100

    Update page.tsx

--- app/juicebox/page.tsx
+++ app/juicebox/page.tsx
@@ -1,75 +1,36 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import InstrumentForge from "@/components/InstrumentForge";
-import Link from "next/link";
 
-export default function JuiceboxHome() {
+export default function JuiceboxPage() {
+
+  const router = useRouter();
 
   return (
 
-    <main className="
-      min-h-screen
-      bg-black
-      p-4
-      text-white
-      relative
-    ">
+    <main className="min-h-screen bg-black p-6">
 
-      {/* Back button */}
+      <button
+        onClick={()=>{
+          router.push("/?portfolio=true");
+        }}
 
-      <Link
-        href="/"
         className="
-        absolute
-        top-6
-        left-6
         border
         border-[#00FF41]/30
         px-4
         py-2
         text-[#00FF41]
-        hover:bg-[#00FF41]/10
-        transition-all
         font-mono
+        mb-4
+        hover:bg-[#00FF41]/10
         "
       >
         ← Back
-      </Link>
+      </button>
 
-
-      <div className="
-        flex
-        justify-center
-        items-center
-        min-h-screen
-      ">
-
-        <div className="
-          w-full
-          max-w-6xl
-        ">
-
-          <InstrumentForge/>
-
-        </div>
-
-      </div>
-
-
-      <footer
-        className="
-        text-center
-        text-[10px]
-        text-[#00FF41]
-        opacity-40
-        font-mono
-        pb-6
-        "
-      >
-
-        JUICEBOX ENGINE // CORE_V1.0
-
-      </footer>
+      <InstrumentForge/>
 
     </main>
 
